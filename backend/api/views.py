92d50f2ffe729b385ed32750024642e145f07b20
Egor1025 <egor20051025@gmail.com>
2025-12-21 05:19:18 +0300

    Configure launch

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,2 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-class PingView(APIView):
-    def get(self, request):
-        return Response({"message": "pong"})
